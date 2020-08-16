1a5d841c29f7bb47089c1c940be60ef7dc12c9a5
GambetaClub <marigyt.maj@gmail.com>
2020-08-16 15:12:07 -0400

    Profile page media responsive and SCSS efficienty. Also, ReadME.md.

---
+++ README.md
@@ -0,0 +1,39 @@
+# Capstone
+
+This website was created for completing CS50W Harvard course. The website's main goal is to help people find others who are travelling to the same place for sharing the same car. Since the website is focused on the United States users, it has all the States and Cities of the country. Some of the features this website has are:
+
+- User sessions.
+- Profile page.
+- Send and receive messages.
+- Creating and deleting trips.
+- Searching trips by many filters.
+- Listing and delisting for/from a trip.
+- User's trips page.
+
+### Smaller features the website has:
+
+- Typing effect in the main page.
+- Use of API's to get user's profile picture from Instagram.
+- Use of HttpResponses for no loading the whole page again.
+- Loading cities corresponding to the selected state in Create Trip tab.
+- Syncronazing messages with Timezone.
+- Use of Swal alerts with HttpResponses
+
+### This project uses:
+
+- Python
+- JavaScript
+- Django
+- CSS
+- HTML
+- SCSS
+
+### Requirements:
+
+- FontAwesome
+- SweetAlerts
+- JQuery (3.5.1)
+- Bootstrap (4.4.1)
+- TheaterJS
+- Popper (1.12.9)
+- Bootstrap Select (1.13.1)
--- capstone/views.py
+++ capstone/views.py
@@ -12,15 +12,17 @@ from .models import User, UsStates, UsCities, Trip, Message
 import pytz
 
 def namedtuplefetchall(cursor):
-    "Return all rows from a cursor as a namedtuple"
+    "Function which returns all rows from a cursor as a namedtuple"
     desc = cursor.description
     nt_result = namedtuple('Result', [col[0] for col in desc])
     return [nt_result(*row) for row in cursor.fetchall()]
 
 def index(request):
+    # Index page
     return render(request, "capstone/index.html")
 
 def login_view(request):
+    # Login page.
     if request.method == "POST":
 
         # Attempt to sign user in
@@ -41,11 +43,13 @@ def login_view(request):
 
 
 def logout_view(request):
+    # Logout user
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
 
 def register(request):
+    # Register user
     if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
@@ -73,6 +77,7 @@ def register(request):
 
 @login_required
 def user_extra_info(request):
+    # Second part of registration. This is optional information. 
     if request.method == 'POST':
         
         f_name = request.POST["first_name"]
@@ -82,7 +87,10 @@ def user_extra_info(request):
         profession = request.POST["profession"]
         interests = request.POST["interests"]
         
+        # Getting the user object.
         user = User.objects.get(username=request.user.username)
+        
+        # Checking if the input where empty and updating user infomration.
         if f_name is not None or f_name != '':
             user.first_name = f_name
         if l_name is not None or l_name != '':
@@ -103,6 +111,7 @@ def user_extra_info(request):
     
 @login_required
 def set_timezone(request):
+    # Sets timezone for synchronazing messages timestamp.
     if request.method == 'POST':
         request.session['django_timezone'] = request.POST['timezone']
         return redirect('/')
@@ -111,6 +120,7 @@ def set_timezone(request):
 
 @login_required
 def profile(request, user_username):
+    # Returns user information and displays its profile page.
     user = User.objects.get(username=user_username)
     if user is None:
         return render(request, "capstone/error.html", {
@@ -123,6 +133,7 @@ def profile(request, user_username):
 
 @login_required
 def messages(request):
+    # Returns last 12 messages from the user from newer to older.
     return render(request, "capstone/messages.html", {
         "users": User.objects.all(),
         "received_messages": Message.objects.filter(receiver=request.user).order_by('-id')[:12],
@@ -131,6 +142,7 @@ def messages(request):
     
 @login_required
 def send_message(request):
+    # Creates a new message and returs messages page.
     if request.method == "POST":
         msg_content = request.POST['content']
         msg_receiver_id = request.POST.get('receiver')
@@ -138,12 +150,14 @@ def send_message(request):
         new_msg.save()
         return HttpResponseRedirect(reverse("messages"))
     else:
+    # Just to make sure no one can send a message with a get request.
         return render(request, "capstone/error.html", {
             "message": "You cannot send messages with a get request"
         })
     
     
 def trips(request):
+    # Returns only future trips and orders them by date. 
     return render(request, "capstone/trips.html",{
         "states": UsStates.objects.all(),
         "trips": Trip.objects.filter(date__gte=date.today()).order_by('date')
@@ -151,8 +165,9 @@ def trips(request):
     
     
 def user_trips(request):
+    # Returns trips which the user either is a passenger or driver.
    user = request.user
-   trips = Trip.objects.filter(date__gte=date.today()).order_by('-id')
+   trips = Trip.objects.filter(date__gte=date.today()).order_by('date')
    return render(request,"capstone/user_trips.html", {
        "p_trips": trips.filter(passengers=user),
        "d_trips":  trips.filter(driver=user)
@@ -160,13 +175,18 @@ def user_trips(request):
 
     
 def search_trip(request):
-    qs = Trip.objects.filter(date__gte=date.today()).order_by('-id')
+    # Handles search by checking which fields were used and adding a filter to main query.
     
+    # Main query (Only future trips and ordered by date)
+    qs = Trip.objects.filter(date__gte=date.today()).order_by('date')
+    
+    # Getting inputs 
     o_state = request.GET.get('o_state')
     o_city = request.GET.get('o_city')
     d_state = request.GET.get('d_state')
     d_city = request.GET.get('d_city')
     
+    #Checking whether the input was filled. If so, it adds a filter to the main query depending on the input.
     if o_state != '' and o_state is not None:
         qs = qs.filter(origin__id_state__state_name__icontains=o_state)
     
@@ -184,8 +204,13 @@ def search_trip(request):
     })
 
 def trip(request, trip_id):
+    # Displays trip details.
     trip_obj = Trip.objects.get(id=trip_id)
     trip_passengers = trip_obj.passengers.all()
+    
+    # Checking if the user is also the trip driver. Depending on this,
+    # on one hand displays delete trip or the option to list or unlist
+    # from the trip.
     if request.user == trip_obj.driver:
             return render(request, "capstone/trip.html", {
                 "self": True,
@@ -202,15 +227,21 @@ def trip(request, trip_id):
             return render(request, "capstone/trip.html", {
                 "trip": trip_obj
             })
-
+            
+@login_required
 def create_trip(request):
-    if request.method == "POST":    ##Reference: 'o' stands for 'origin' and 'd' for destination.
-        o_state_id = request.POST["o_state"] ##Getting the id of the state.
-        o_city_name = request.POST.get("o_city") ##Getting the name of the city.
+    # Creates a new trip.
+
+    # If the user submitted a form it tries to create a trip.
+    if request.method == "POST":    # Reference: 'o' stands for 'origin' and 'd' for destination.
+        # Selection part.
+        o_state_id = request.POST["o_state"] # Getting the id of the state.
+        o_city_name = request.POST.get("o_city") # Getting the name of the city.
         o_city_obj = UsCities.objects.filter(id_state=o_state_id, city=o_city_name).first()
-        d_state_id = request.POST["d_state"] ##Getting the id of the state.
-        d_city_name = request.POST.get("d_city") ##Getting the name of the city.
+        d_state_id = request.POST["d_state"] # Getting the id of the state.
+        d_city_name = request.POST.get("d_city") # Getting the name of the city.
         d_city_obj = UsCities.objects.filter(id_state=d_state_id, city=d_city_name).first()
+        
         date = request.POST["date_picker"]
         time = request.POST["time_picker"]
         est_time = request.POST["est_time"]
@@ -227,60 +258,74 @@ def create_trip(request):
         
 @login_required
 def delete_trip(request, trip_id):
+    # Deletes a trip.
     trip_obj = Trip.objects.get(id=trip_id)
+    # Checking if the user is the driver of the trip since it should be the only one with the permission.
     if trip_obj.driver == request.user:
         trip_obj.delete()
         return render(request, "capstone/success.html", {
             "message": "You have succesfully deleted the trip."
         })
+    # If not, displays an error.
     else:
         return render(request, "capstone/error.html", {
             "message": "You cannot delete a someone's else trip."
         })
- 
-def get_cities(request,state_id):
-    cities = list(UsCities.objects.filter(id_state=state_id))
-    return HttpResponse(cities)
 
 @login_required
 def add_passenger(request, trip_id):
+    # Function that removes or add a passenger from/to certain trip.
     trip_obj = Trip.objects.get(id=trip_id)
     user = request.user
-    if trip_obj.avai_seats > 0:
-        if trip_obj.passengers.filter(id=user.id).exists():
-            trip_obj.passengers.remove(user)
-            trip_obj.avai_seats += 1
-            trip_obj.save()
-        else:
+    # Checking if the user is already a passanger of such trip.
+    if trip_obj.passengers.filter(id=user.id).exists():
+        # If it is, removes the passenger from the trip.
+        trip_obj.passengers.remove(user)
+        trip_obj.avai_seats += 1
+        trip_obj.save()
+    # If it's not, it adds the passenger.
+    else:
+        if trip_obj.avai_seats > 0:
             trip_obj.passengers.add(user)
             trip_obj.avai_seats -= 1
             trip_obj.save()
-    else:
-        return HttpResponseForbidden('This trip has no seats available.')
-    
+         # If it doesn't have seats available, it displays message.
+        else:
+            return HttpResponseForbidden('This trip has no seats available.')
+
+def full_trip(request):
+    # Function that displays message.
+    return render(request, "capstone/error.html", {
+        "message": "This trips has no seats available"
+    })
+   
+def get_cities(request,state_id):
+    # Gets the cities from cetain state. (It is for create trip page selection input)
+    cities = list(UsCities.objects.filter(id_state=state_id))
+    return HttpResponse(cities)
     
 def get_passengers(request, trip_id):
+    # Function that gets passengers from certain trip. (It is for trip page)
     trip_obj = Trip.objects.filter(id=trip_id).first()
     return HttpResponse(trip_obj.passengers.count())
 
 def get_seats(request, trip_id):
+    # Function that gets seats from certain trip. (It is for trip page)
     trip_obj = Trip.objects.get(id=trip_id)
     return HttpResponse(trip_obj.avai_seats)
 
 @login_required
 def read_message(request, message_id):
+    # Function that reads a message.
     message = Message.objects.get(id=message_id)
+    # Checking if the user is the receiver of the trip. It should be the only one with the permission to do it.
     if request.user == message.receiver:
         message.read = True
         message.save()
         return HttpResponse(True)
+    # If it's not, it displays an error.
     else:
         return render(request, "cpastone/error.html",{
             "message": "You cannot read someone's else message."
         })
-       
-def full_trip(request):
-    return render(request, "capstone/error.html", {
-        "message": "This trips has no seats available"
-    })
     
